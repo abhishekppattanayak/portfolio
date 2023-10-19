8457acb5831e425772b5e9f943e2626f394dd5a2
Abhishek Pattanayak <2021.abhishek.pattanayak@ves.ac.in>
2023-10-19 21:57:16 +0530

    Update website

--- script/scipt.js
+++ script/scipt.js
@@ -1,12 +1,43 @@
 const intro = document.getElementById('intro');
+const list = "a student.    | a web enthusiast.    | passionate for AI and ML.    ".split('|');
+let currentIndex = 0;
 
-const props = "a student| a web enthusiast| passionate for AI".split('|');
-const text = "Hi! I am ";
-let i = 0;
-
-function updateIntroText() {
-  intro.textContent = text + props[i] + '.';
-  i = (i + 1) % props.length; 
+function typeText(textElement, text, speed, callback) {
+  let index = 0;
+  let timer = setInterval(function() {
+    if (index < text.length) {
+      textElement.textContent += text.charAt(index);
+      index++;
+    } else {
+      clearInterval(timer);
+      if (callback) callback();
+    }
+  }, speed);
 }
 
-setInterval(updateIntroText, 1300);
+function backspaceText(textElement, text, deleteSpeed, callback) {
+  let index = text.length - 1;
+  let timer = setInterval(function() {
+    if (index >= "Hi! I am ".length) {
+      textElement.textContent = textElement.textContent.substring(0, textElement.textContent.length - 1);
+      index--;
+    } else {
+      clearInterval(timer);
+      if (callback) callback();
+    }
+  }, deleteSpeed);
+}
+
+function animateText() {
+  if (currentIndex < list.length) {
+    const listItem = list[currentIndex];
+    typeText(intro, listItem, 120, function() {
+      backspaceText(intro, "Hi! I am " + listItem, 100, function() {
+        currentIndex = (currentIndex + 1) % list.length;
+        animateText();
+      });
+    });
+  }
+}
+
+animateText();
